3704b175e874910c0921a219c4229caeb371aed4
dan <dancr.wley@gmail.com>
2023-03-29 23:39:31 -0500

    red go green

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Write your code here!
+const element = document.getElementById("main");
+element.remove();
+let newHeader = document.createElement("h1");
+newHeader.setAttribute('id','victory');
+newHeader.textContent = "dan is the champion."
+document.body.append(newHeader);
